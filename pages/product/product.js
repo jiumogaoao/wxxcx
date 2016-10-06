718462d0f04ca49747739bee89982611a953aed6
jiumogaoao <394127821@qq.com>
2016-10-06 09:53:13 +0800

    change
    change

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,6 +1,19 @@
 var app=getApp();
 Page({
   data: {
+    imgUrls: [
+      '../../images/1.jpg',
+      '/images/2.jpg',
+      '/images/3.jpg'
+    ],
+    name:"商品1",
+    dsc:"描述描述描述描述",
+    price:99,
+    count:1,
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
   onLoad: function () {
       console.log(app.globalData.productId)
